1b4b05dc1a30f4762aa29fc75ea479ecbb693daa
SilasBach <79838393+SilasBach@users.noreply.github.com>
2023-12-19 09:57:52 +0000

    fast api running

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -7,8 +7,7 @@ set -x
 cd frontend && npm install && cd ..
 
 # install python dependencies in subdirectories
-poetry config virtualenvs.in-project true
-cd backend && poetry env use python3.11 && poetry install --no-root && cd ..
-
+poetry config virtualenvs.in-project false
+cd backend && poetry env use python3.11 && poetry install --no-root && uvicorn main:app --reload && cd ..
 
 exit 0
